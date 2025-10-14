febb4e763a0ae2e5fb350d232beefbe727450eb5
DerpyMario <88537784+DerpyMario@users.noreply.github.com>
2025-10-14 13:13:01 +0300

    Update macos.sh

--- macos.sh
+++ macos.sh
@@ -1,33 +1,45 @@
-#macos.sh MAC_USER_PASSWORD VNC_PASSWORD NGROK_AUTH_TOKEN MAC_REALNAME
+#!/bin/bash
 
-#disable spotlight indexing
+# Move to a safe directory to avoid permission errors
+cd /tmp
+
+# Parameters
+MAC_USER_PASSWORD="$1"
+VNC_PASSWORD="$2"
+NGROK_AUTH_TOKEN="$3"
+MAC_REALNAME="$4"
+
+# Disable Spotlight indexing
 sudo mdutil -i off -a
 
-#Create new account
+# Create new account
 sudo dscl . -create /Users/tcv
 sudo dscl . -create /Users/tcv UserShell /bin/bash
-sudo dscl . -create /Users/tcv RealName $4
+sudo dscl . -create /Users/tcv RealName "$MAC_REALNAME"
 sudo dscl . -create /Users/tcv UniqueID 1001
 sudo dscl . -create /Users/tcv PrimaryGroupID 80
 sudo dscl . -create /Users/tcv NFSHomeDirectory /Users/tcv
-sudo dscl . -passwd /Users/tcv $1
-sudo dscl . -passwd /Users/tcv $1
+sudo dscl . -passwd /Users/tcv "$MAC_USER_PASSWORD"
 sudo createhomedir -c -u tcv > /dev/null
-sudo dscl . -append /Groups/admin GroupMembership username
+sudo dscl . -append /Groups/admin GroupMembership tcv
 
-#Enable VNC
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -allUsers -privs -all
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -clientopts -setvnclegacy -vnclegacy yes 
+# Enable Remote Management for the user
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart \
+    -activate -configure -access -on -users tcv -privs -all \
+    -restart -agent -menu
 
-echo $2 | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
+# Enable VNC with password
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart \
+    -configure -clientopts -setvnclegacy -vnclegacy yes
 
-#Start VNC/reset changes
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent -console
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
+echo "$VNC_PASSWORD" | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | \
+    sudo tee /Library/Preferences/com.apple.VNCSettings.txt
 
-#install ngrok
+# Install ngrok
 brew install --cask ngrok
 
-#configure ngrok and start it
-ngrok authtoken $3
+# Configure ngrok and start it
+ngrok authtoken "$NGROK_AUTH_TOKEN"
 ngrok tcp 5900 --region=ap &
+
+echo "Setup complete. If you see a black screen in VNC, log into the GUI as 'tcv' first, or grant Screen Recording permissions in System Preferences."
